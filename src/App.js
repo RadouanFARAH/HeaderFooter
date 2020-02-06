b0ce0b98165287ac37c1091fb493efc95aa3b9ef
Redouane FARAH <redouan-farah@outlook.fr>
2020-02-06 23:39:37 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {Nav, Navbar} from 'react-bootstrap' 
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar.Brand href="/">Urbateur</Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="ml-auto">
+        <Nav.Link style={{backgroundColor:'white', color:'black'}} href="#deets">Qui Somme Nous</Nav.Link>
+        <Nav.Link style={{backgroundColor:'white', color:'black'}}  eventKey={2} href="#memes">
+          Nous-Contacter
+        </Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+  <footer className="footer text-center">
+    <div className="container">
+      <p className="footer-text">Footer</p>
     </div>
+  </footer>
+  </div>
   );
 }
 
